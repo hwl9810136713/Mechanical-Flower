b90e7288b46bf2a57e3be6afcf919537ea26b2d5
Marc Edward <hwl981013@gmail.com>
2024-04-28 23:49:03 +0800

    初始的后端项目提交

---
+++ Mechanical-Flower-Server/Mechanical-Flower-Server/Mechanical-Flower-Module-Server/src/main/java/com/jarvisServer04/mechanicalflowermoduleserver/MechanicalFlowerModuleServerApplication.java
@@ -0,0 +1,13 @@
+package com.jarvisServer04.mechanicalflowermoduleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicalFlowerModuleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechanicalFlowerModuleServerApplication.class, args);
+    }
+
+}
---
+++ Mechanical-Flower-Server/Mechanical-Flower-Server/Mechanical-Flower-Module-Server/src/test/java/com/jarvisServer04/mechanicalflowermoduleserver/MechanicalFlowerModuleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvisServer04.mechanicalflowermoduleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicalFlowerModuleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
